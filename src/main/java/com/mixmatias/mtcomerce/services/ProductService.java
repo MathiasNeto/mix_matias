bfc399f11c261f92182239a96b8c09a452b0cbbc
Emanuel Matias <matias.neto.edu@gmail.com>
2023-06-27 14:39:30 -0300

    GET Products paged

--- src/main/java/com/mixmatias/mtcomerce/services/ProductService.java
+++ src/main/java/com/mixmatias/mtcomerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.mixmatias.mtcomerce.dto.ProductDTO;
 import com.mixmatias.mtcomerce.entities.Product;
 import com.mixmatias.mtcomerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,6 +22,10 @@ public class ProductService {
     public ProductDTO findById(Long id){
         Optional<Product> result = productRepository.findById(id);
         return new ProductDTO(result.get());
+    }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return productRepository.findAll(pageable).map(ProductDTO::new);
     }
 
 }
