6d9ef7db133777f5f0b839577f25d61b77ed2591
Emanuel Matias <matias.neto.edu@gmail.com>
2023-08-04 10:59:44 -0300

    Update ClientDTO

---
+++ src/main/java/com/mixmatias/mtcomerce/dto/ClientDTO.java
@@ -0,0 +1,28 @@
+package com.mixmatias.mtcomerce.dto;
+
+
+import com.mixmatias.mtcomerce.entities.User;
+
+public class ClientDTO {
+
+	private Long id;
+	private String name;
+	
+	public ClientDTO(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public ClientDTO(User entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
