f821ea5c4b7e34ccb63bd124a88c01b6f3b10b6d
Emanuel Matias <matias.neto.edu@gmail.com>
2023-08-04 10:50:33 -0300

    Spring Security

---
+++ src/main/java/com/mixmatias/mtcomerce/services/exceptions/ForbiddenException.java
@@ -0,0 +1,9 @@
+package com.mixmatias.mtcomerce.services.exceptions;
+
+@SuppressWarnings("serial")
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String msg) {
+        super(msg);
+    }
+}
